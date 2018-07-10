7239f13d1a2283fb89169d5eaf2c478843078e21
Abel Pinheiro de Figueiredo <abelpinheiro95@gmail.com>
2018-07-10 00:30:20 -0300

    initial commit TabLayout implementado

---
+++ app/src/main/java/abelpinheiro/github/io/fortalezatourguide/CategoryAdapter.java
@@ -0,0 +1,49 @@
+package abelpinheiro.github.io.fortalezatourguide;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        this.mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0){
+            return new BeachFragment();
+        }else if (position == 1){
+            return new AttractionsFragment();
+        }else if (position == 2){
+            return new ShoppingFragment();
+        }else{
+            return new FoodFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0){
+            return mContext.getString(R.string.beach_title);
+        }else if (position == 1){
+            return mContext.getString(R.string.attractions_title);
+        }else if (position == 2){
+            return mContext.getString(R.string.shopping_title);
+        }else{
+            return mContext.getString(R.string.food_title);
+        }
+    }
+}
